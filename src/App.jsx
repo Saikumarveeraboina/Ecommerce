976bd28a42c9359dd5902bbf9b503a4ed7acbca5
Saikumar <saikumar96030433@gmail.com>
2025-12-04 19:55:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,17 @@ import StoreNavbar from './Components/Navbar/Navbar';
 import Product from './Pages/Product/Product';
 import CartPage from './Pages/CartPage/Cart';
 import { Routes, Route } from 'react-router-dom';
+
+
 function App() {
   return (
     <div style={{ overflow: 'hidden' }}>
       <StoreNavbar />
-
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/Product/:id' element={<Product/>} />
         <Route path='/MyCart' element={<CartPage/>} />
       </Routes>
-      {/* <Product /> */}
-      {/* <Home /> */}
     </div>
   )
 }
