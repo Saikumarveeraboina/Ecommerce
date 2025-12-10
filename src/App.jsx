240f8e1bea86cf9b22d1cba66701eef03b0fcbb1
Saikumar <saikumar96030433@gmail.com>
2025-12-10 09:55:22 +0530

    Remove unnecessary blank lines in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import StoreNavbar from './Components/Navbar/Navbar';
 import Product from './Pages/Product/Product';
 import CartPage from './Pages/CartPage/Cart';
 import { Routes, Route } from 'react-router-dom';
-
-
 function App() {
   return (
     <div style={{ overflow: 'hidden' }}>
